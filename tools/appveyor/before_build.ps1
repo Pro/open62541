60f93ea1ef7563075cb8594f5313d6990cdef6b2
Stefan Profanter <git@s.profanter.me>
2017-11-24 13:25:25 +0100

    Fixup 18

--- tools/appveyor/before_build.ps1
+++ tools/appveyor/before_build.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = "Stop"
 
 # use MinGW64
-[Environment]::SetEnvironmentVariable("Path", "C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\bin;" + $env:Path, [EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("Path", "C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\bin;" + $env:Path, [EnvironmentVariableTarget]::Machine)
 
 # Workaround for CMake not wanting sh.exe on PATH for MinGW
-[Environment]::SetEnvironmentVariable("Path", $env:Path.replace("C:\Program Files\Git\usr\bin;",""), [EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("Path", $env:Path.replace("C:\Program Files\Git\usr\bin;",""), [EnvironmentVariableTarget]::Machine)
 
 # Miktex
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";c:\miktex\texmfs\install\miktex\bin", [EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";c:\miktex\texmfs\install\miktex\bin", [EnvironmentVariableTarget]::Machine)
 # We also need miktex for the following script
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";c:\miktex\texmfs\install\miktex\bin", [EnvironmentVariableTarget]::Process)
 # autoinstall latex packages (0=no, 1=autoinstall, 2=ask)
@@ -16,7 +16,11 @@ $ErrorActionPreference = "Stop"
 & initexmf --update-fndb
 
 # Python
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:APPDATA\Python\Scripts", [EnvironmentVariableTarget]::User)
+
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:APPDATA\Python\Scripts", [EnvironmentVariableTarget]::Machine)
 
 # DrMemory
-#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files (x86)\Dr. Memory\bin", [EnvironmentVariableTarget]::User)
+#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files (x86)\Dr. Memory\bin", [EnvironmentVariableTarget]::Machine)
+
+
+echo "PATH=$env:PATH"
