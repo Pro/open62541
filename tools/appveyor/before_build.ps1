5f67dec9a29480ab14b4e5456120bb6166be5afa
Stefan Profanter <git@s.profanter.me>
2017-11-24 14:52:52 +0100

    Fixup 20

--- tools/appveyor/before_build.ps1
+++ tools/appveyor/before_build.ps1
@@ -2,14 +2,12 @@ $ErrorActionPreference = "Stop"
 refreshenv
 
 # use MinGW64
-[Environment]::SetEnvironmentVariable("Path", "C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\bin;" + $env:Path, [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", "C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\bin;" + $env:Path, [EnvironmentVariableTarget]::Process)
 
 # Workaround for CMake not wanting sh.exe on PATH for MinGW
-[Environment]::SetEnvironmentVariable("Path", $env:Path.replace("C:\Program Files\Git\usr\bin;",""), [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", $env:Path.replace("C:\Program Files\Git\usr\bin;",""), [EnvironmentVariableTarget]::Process)
 
 # Miktex
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";c:\miktex\texmfs\install\miktex\bin", [EnvironmentVariableTarget]::Machine)
-# We also need miktex for the following script
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";c:\miktex\texmfs\install\miktex\bin", [EnvironmentVariableTarget]::Process)
 # autoinstall latex packages (0=no, 1=autoinstall, 2=ask)
 # this adds this to the registry!
@@ -18,10 +16,10 @@ refreshenv
 
 # Python
 
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:APPDATA\Python\Scripts", [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:APPDATA\Python\Scripts", [EnvironmentVariableTarget]::Process)
 
 # DrMemory
-#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files (x86)\Dr. Memory\bin", [EnvironmentVariableTarget]::Machine)
+#[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files (x86)\Dr. Memory\bin", [EnvironmentVariableTarget]::Process)
 
 
 echo "PATH=$env:PATH"
