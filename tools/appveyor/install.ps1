53ed3eb39ac65beab713908bc1209a67aef91cb7
Stefan Profanter <git@s.profanter.me>
2017-11-24 16:47:16 +0100

    Fixup 35

--- tools/appveyor/install.ps1
+++ tools/appveyor/install.ps1
@@ -2,17 +2,19 @@ $ErrorActionPreference = "Stop"
 
 $make_cmd = "& dir C:\"
 Invoke-Expression $make_cmd
-echo "Exit1 = $LASTEXITCODE"
 if ($LASTEXITCODE -ne 0) {
+	echo "Failed dir"
 	exit $LASTEXITCODE
 }
+echo "Exit1 = $LASTEXITCODE"
 
 $make_cmd = "& $env:MAKE"
 Invoke-Expression $make_cmd
-echo "Exit2 = $LASTEXITCODE"
 if ($LASTEXITCODE -ne 0) {
+	echo "Failed make"
 	exit $LASTEXITCODE
 }
+echo "Exit2 = $LASTEXITCODE"
 
 & git submodule --quiet update --init --recursive
 
