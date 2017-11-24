0176830a9c85c0a194412691155ced1d5d10a6b2
Stefan Profanter <git@s.profanter.me>
2017-11-24 12:34:31 +0100

    Fixup 11

--- tools/appveyor/build.ps1
+++ tools/appveyor/build.ps1
@@ -2,39 +2,39 @@ $ErrorActionPreference = "Stop"
 
 cd c:\projects\open62541
 # Collect files for .zip packing
-New-Item -ItemType directory -Path "pack"
-Copy-Item "$env:APPVEYOR_BUILD_FOLDER\LICENSE" pack\
-Copy-Item "$env:APPVEYOR_BUILD_FOLDER\AUTHORS" pack\
-Copy-Item ""$env:APPVEYOR_BUILD_FOLDER\README.md" pack\
+New-Item -ItemType directory -Path "$env:APPVEYOR_BUILD_FOLDER\pack"
+Copy-Item "$env:APPVEYOR_BUILD_FOLDER\LICENSE" $env:APPVEYOR_BUILD_FOLDER\pack\
+Copy-Item "$env:APPVEYOR_BUILD_FOLDER\AUTHORS" $env:APPVEYOR_BUILD_FOLDER\pack\
+Copy-Item ""$env:APPVEYOR_BUILD_FOLDER\README.md" $env:APPVEYOR_BUILD_FOLDER\pack\
 
 echo "`n##### Building Documentation on $env:CC_NAME #####`n"
-New-Item -ItemType directory -Path "build"
+New-Item -ItemType directory -Path "$env:APPVEYOR_BUILD_FOLDER\build"
 cd build
 cmake -DMIKTEX_BINARY_PATH=c:\miktex\texmfs\install\miktex\bin -DCMAKE_BUILD_TYPE=Release -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -DUA_BUILD_EXAMPLES:BOOL=OFF -G"$env:CC_NAME" ..
 cmake --build . --target doc_latex
 cmake --build . --target doc_pdf
 move "$env:APPVEYOR_BUILD_FOLDER\build\doc_latex\open62541.pdf" $env:APPVEYOR_BUILD_FOLDER\pack\
 cd ..
-Remove-Item -Path build -Recurse
+Remove-Item -Path $env:APPVEYOR_BUILD_FOLDER\build -Recurse
 
 echo "`n##### Testing $env:CC_NAME #####`n"
-New-Item -ItemType directory -Path "build"
+New-Item -ItemType directory -Path "$env:APPVEYOR_BUILD_FOLDER\build"
 cd build
 cmake -DUA_BUILD_EXAMPLES:BOOL=ON -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -G"$env:CC_NAME" ..
 $env:MAKE
 cd ..
-Remove-Item -Path build -Recurse
+Remove-Item -Path $env:APPVEYOR_BUILD_FOLDER\build -Recurse
 
 echo "`n##### Testing $env:CC_NAME with full NS0 #####`n"
-New-Item -ItemType directory -Path "build"
+New-Item -ItemType directory -Path "$env:APPVEYOR_BUILD_FOLDER\build"
 cd build
 cmake -DUA_BUILD_EXAMPLES:BOOL=ON -DUA_ENABLE_FULL_NS0:BOOL=ON -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -G"$env:CC_NAME" ..
 $env:MAKE
 cd ..
-Remove-Item -Path build -Recurse
+Remove-Item -Path $env:APPVEYOR_BUILD_FOLDER\build -Recurse
 
 echo "`n##### Testing $env:CC_NAME with amalgamation #####`n"
-New-Item -ItemType directory -Path "build"
+New-Item -ItemType directory -Path "$env:APPVEYOR_BUILD_FOLDER\build"
 cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_BUILD_EXAMPLES:BOOL=ON -DUA_ENABLE_AMALGAMATION:BOOL=ON -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -DBUILD_SHARED_LIBS:BOOL=OFF -G"$env:CC_NAME" ..
 $env:MAKE
@@ -50,10 +50,10 @@ if ($env:CC_SHORTNAME -eq "mingw") {
 cd ..
 7z a -tzip open62541-$env:CC_SHORTNAME-static.zip "$env:APPVEYOR_BUILD_FOLDER\pack\*" "$env:APPVEYOR_BUILD_FOLDER\pack_tmp\*"
 Remove-Item -Path pack_tmp -Recurse
-Remove-Item -Path build -Recurse
+Remove-Item -Path $env:APPVEYOR_BUILD_FOLDER\build -Recurse
 
 echo "`n##### Testing $env:CC_NAME with amalgamation and .dll #####`n"
-New-Item -ItemType directory -Path "build"
+New-Item -ItemType directory -Path "$env:APPVEYOR_BUILD_FOLDER\build"
 cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_BUILD_EXAMPLES:BOOL=ON -DUA_ENABLE_AMALGAMATION:BOOL=ON -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -DBUILD_SHARED_LIBS:BOOL=ON -G"$env:CC_NAME" ..
 $env:MAKE
@@ -72,11 +72,11 @@ if ($env:CC_SHORTNAME -eq "mingw") {
 cd ..
 7z a -tzip open62541-$env:CC_SHORTNAME-dynamic.zip "$env:APPVEYOR_BUILD_FOLDER\pack\*" "$env:APPVEYOR_BUILD_FOLDER\pack_tmp\*"
 Remove-Item -Path pack_tmp -Recurse
-Remove-Item -Path build -Recurse
+Remove-Item -Path $env:APPVEYOR_BUILD_FOLDER\build -Recurse
 
 if ($env:CC_SHORTNAME -eq "vs2015") {
 	# Only execute unit tests on vs2015 to save compilation time
-	New-Item -ItemType directory -Path "build"
+	New-Item -ItemType directory -Path "$env:APPVEYOR_BUILD_FOLDER\build"
 	cd build
 	echo "`n##### Testing $env:CC_NAME with unit tests #####`n"
 	cmake -DCMAKE_BUILD_TYPE=Debug -DUA_BUILD_EXAMPLES=OFF -DUA_ENABLE_DISCOVERY=ON -DUA_ENABLE_DISCOVERY_MULTICAST=ON -DUA_BUILD_UNIT_TESTS=ON -DUA_ENABLE_UNIT_TESTS_MEMCHECK=ON  -DCMAKE_LIBRARY_PATH=c:\check\lib -DCMAKE_INCLUDE_PATH=c:\check\include -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -G"$env:CC_NAME" ..
