f38659fc3a26dce6c91445e557ccfdfea779d358
Stefan Profanter <git@s.profanter.me>
2017-11-24 17:09:15 +0100

    Build mingw

--- tools/appveyor/build.ps1
+++ tools/appveyor/build.ps1
@@ -10,19 +10,19 @@ Copy-Item LICENSE pack
 Copy-Item AUTHORS pack
 Copy-Item README.md pack
 
-echo "`n###################################################################"
-echo "`n##### Building Documentation on $env:CC_NAME #####`n"
+Write-Host -ForegroundColor Green "`n###################################################################"
+Write-Host -ForegroundColor Green "`n##### Building Documentation on $env:CC_NAME #####`n"
 New-Item -ItemType directory -Path build
 cd build
 & cmake -DMIKTEX_BINARY_PATH=c:\miktex\texmfs\install\miktex\bin -DCMAKE_BUILD_TYPE=Release -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -DUA_BUILD_EXAMPLES:BOOL=OFF -G"$env:CC_NAME" ..
 & cmake --build . --target doc_latex
 if ($LASTEXITCODE -and $LASTEXITCODE -ne 0) {
-	echo "`n`n*** Make doc_latex. Exiting ... ***"
+	Write-Host -ForegroundColor Red "`n`n*** Make doc_latex. Exiting ... ***"
 	exit $LASTEXITCODE
 }
 & cmake --build . --target doc_pdf
 if ($LASTEXITCODE -and $LASTEXITCODE -ne 0) {
-	echo "`n`n*** Make doc_pdf. Exiting ... ***"
+	Write-Host -ForegroundColor Red "`n`n*** Make doc_pdf. Exiting ... ***"
 	exit $LASTEXITCODE
 }
 cd ..
@@ -30,42 +30,42 @@ Move-Item -Path "build\doc_latex\open62541.pdf" -Destination pack\
 Remove-Item -Path build -Recurse
 
 
-echo "`n###################################################################"
-echo "`n##### Testing $env:CC_NAME #####`n"
+Write-Host -ForegroundColor Green "`n###################################################################"
+Write-Host -ForegroundColor Green "`n##### Testing $env:CC_NAME #####`n"
 New-Item -ItemType directory -Path "build"
 cd build
 & cmake -DUA_BUILD_EXAMPLES:BOOL=ON -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -G"$env:CC_NAME" ..
 Invoke-Expression $make_cmd
 if ($LASTEXITCODE -and $LASTEXITCODE -ne 0) {
-	echo "`n`n*** Make failed. Exiting ... ***"
+	Write-Host -ForegroundColor Red "`n`n*** Make failed. Exiting ... ***"
 	exit $LASTEXITCODE
 }
 cd ..
 Remove-Item -Path build -Recurse
 
 
-echo "`n###################################################################"
-echo "`n##### Testing $env:CC_NAME with full NS0 #####`n"
+Write-Host -ForegroundColor Green "`n###################################################################"
+Write-Host -ForegroundColor Green "`n##### Testing $env:CC_NAME with full NS0 #####`n"
 New-Item -ItemType directory -Path "build"
 cd build
 & cmake -DUA_BUILD_EXAMPLES:BOOL=ON -DUA_ENABLE_FULL_NS0:BOOL=ON -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -G"$env:CC_NAME" ..
 Invoke-Expression $make_cmd
 if ($LASTEXITCODE -and $LASTEXITCODE -ne 0) {
-	echo "`n`n*** Make failed. Exiting ... ***"
+	Write-Host -ForegroundColor Red "`n`n*** Make failed. Exiting ... ***"
 	exit $LASTEXITCODE
 }
 cd ..
 Remove-Item -Path build -Recurse
 
 
-echo "`n###################################################################"
-echo "`n##### Testing $env:CC_NAME with amalgamation #####`n"
+Write-Host -ForegroundColor Green "`n###################################################################"
+Write-Host -ForegroundColor Green "`n##### Testing $env:CC_NAME with amalgamation #####`n"
 New-Item -ItemType directory -Path "build"
 cd build
 & cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_BUILD_EXAMPLES:BOOL=ON -DUA_ENABLE_AMALGAMATION:BOOL=ON -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -DBUILD_SHARED_LIBS:BOOL=OFF -G"$env:CC_NAME" ..
 Invoke-Expression $make_cmd
 if ($LASTEXITCODE -and $LASTEXITCODE -ne 0) {
-	echo "`n`n*** Make failed. Exiting ... ***"
+	Write-Host -ForegroundColor Red "`n`n*** Make failed. Exiting ... ***"
 	exit $LASTEXITCODE
 }
 cd ..
@@ -83,14 +83,14 @@ Remove-Item -Path pack_tmp -Recurse
 Remove-Item -Path build -Recurse
 
 
-echo "`n###################################################################"
-echo "`n##### Testing $env:CC_NAME with amalgamation and .dll #####`n"
+Write-Host -ForegroundColor Green "`n###################################################################"
+Write-Host -ForegroundColor Green "`n##### Testing $env:CC_NAME with amalgamation and .dll #####`n"
 New-Item -ItemType directory -Path "build"
 cd build
 & cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUA_BUILD_EXAMPLES:BOOL=ON -DUA_ENABLE_AMALGAMATION:BOOL=ON -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -DBUILD_SHARED_LIBS:BOOL=ON -G"$env:CC_NAME" ..
 Invoke-Expression $make_cmd
 if ($LASTEXITCODE -and $LASTEXITCODE -ne 0) {
-	echo "`n`n*** Make failed. Exiting ... ***"
+	Write-Host -ForegroundColor Red "`n`n*** Make failed. Exiting ... ***"
 	exit $LASTEXITCODE
 }
 cd ..
@@ -114,12 +114,12 @@ if ($env:CC_SHORTNAME -eq "vs2015") {
 	# Only execute unit tests on vs2015 to save compilation time
 	New-Item -ItemType directory -Path "build"
 	cd build
-	echo "`n###################################################################"
-	echo "`n##### Testing $env:CC_NAME with unit tests #####`n"
+	Write-Host -ForegroundColor Green "`n###################################################################"
+	Write-Host -ForegroundColor Green "`n##### Testing $env:CC_NAME with unit tests #####`n"
 	& cmake -DCMAKE_BUILD_TYPE=Debug -DUA_BUILD_EXAMPLES=OFF -DUA_ENABLE_DISCOVERY=ON -DUA_ENABLE_DISCOVERY_MULTICAST=ON -DUA_BUILD_UNIT_TESTS=ON -DUA_ENABLE_UNIT_TESTS_MEMCHECK=ON  -DCMAKE_LIBRARY_PATH=c:\check\lib -DCMAKE_INCLUDE_PATH=c:\check\include -DUA_COMPILE_AS_CXX:BOOL=$env:FORCE_CXX -G"$env:CC_NAME" ..
 	Invoke-Expression $make_cmd
     if ($LASTEXITCODE -and $LASTEXITCODE -ne 0) {
-    	echo "`n`n*** Make failed. Exiting ... ***"
+    	Write-Host -ForegroundColor Red "`n`n*** Make failed. Exiting ... ***"
     	exit $LASTEXITCODE
     }
 	& cmake --build . --target test-verbose --config debug
--- tools/appveyor/install.ps1
+++ tools/appveyor/install.ps1
@@ -3,13 +3,13 @@ $ErrorActionPreference = "Stop"
 & git submodule --quiet update --init --recursive
 
 
-echo "`n### Installing CMake and python ###`n"
+Write-Host -ForegroundColor Green "`n### Installing CMake and python ###`n"
 & cinst --no-progress cmake python2
 
-echo "`n### Installing sphinx ###`n"
+Write-Host -ForegroundColor Green "`n### Installing sphinx ###`n"
 & pip install --user sphinx sphinx_rtd_theme
 
-echo "`n### Installing Miktex ###`n"
+Write-Host -ForegroundColor Green "`n### Installing Miktex ###`n"
 if (-not (Test-Path "c:\miktex\texmfs\install\miktex\bin\pdflatex.exe")) {
 	& appveyor DownloadFile https://ftp.uni-erlangen.de/mirrors/CTAN/systems/win32/miktex/setup/miktex-portable.exe
 	& 7z x miktex-portable.exe -oc:\miktex -bso0 -bsp0
@@ -21,12 +21,12 @@ if (-not (Test-Path "c:\miktex\texmfs\install\miktex\bin\pdflatex.exe")) {
 	Remove-Item -Path c:\miktex\texmfs\install\miktex\bin\a5toa4.exe
 }
 
-echo "`n### Installing graphviz ###`n"
+Write-Host -ForegroundColor Green "`n### Installing graphviz ###`n"
 & cinst --no-progress graphviz.portable
 
-echo "`n### Installing libcheck ###`n"
+Write-Host -ForegroundColor Green "`n### Installing libcheck ###`n"
 & appveyor DownloadFile https://github.com/Pro/check/releases/download/0.12.0_win/check.zip
 & 7z x check.zip -oc:\ -bso0 -bsp0
 
-echo "`n### Installing DrMemory ###`n"
+Write-Host -ForegroundColor Green "`n### Installing DrMemory ###`n"
 & cinst --no-progress drmemory.portable
